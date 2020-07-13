25c8d0fd5d1e4a0c1cdefa625affb969f7503d46
Denys Madureira <denysmb@zoho.com>
2020-07-13 14:03:40 -0300

    Initialize

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+    [SerializeField]
+    private int _yLimit = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float xPosition = transform.position.x;
+        float yPosition = transform.position.y;
+
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (yPosition > _yLimit)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _nextFire = 0.0f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        Movement();
+        Shoot();
+    }
+
+    private void Movement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3((_speed * xInput), (_speed * yInput), 0) * Time.deltaTime);
+
+        float xPosition = transform.position.x;
+        float yPosition = transform.position.y;
+        float limitX = xPosition >= 8 ? 8 : xPosition <= -8 ? -8 : xPosition;
+        float limitY = yPosition >= 4 ? 4 : yPosition <= -4 ? -4 : yPosition;
+
+        if(xPosition >= 8 || xPosition <= -8 || yPosition >= 4 || yPosition <= -4)
+        {
+            transform.position = new Vector3(limitX, limitY, 0);
+        }
+    }
+
+    private void Shoot()
+    {
+        float xPosition = transform.position.x;
+        float yPosition = transform.position.y;
+        bool isFiring = Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0);
+
+        if (isFiring && Time.time > _nextFire)
+        {
+            _nextFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, new Vector3(xPosition, yPosition + 0.9f, 0), Quaternion.identity);
+        }
+    }
+}
